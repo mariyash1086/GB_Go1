ccf58c4c7a03b86e42c615f503d4a0bca1b0bb8e
Mariyash1086 <milly10031986@gmail.com>
2022-01-14 22:28:51 +0400

    Its HW4

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []int{1, 10, 20, 5, 6, 4, 0, 178, 150}
+
+	fmt.Println(slice)
+	sort(slice)
+	fmt.Println(slice)
+
+}
+
+func sort(a []int) {
+
+	for j := 1; j < len(a); j++ {
+		key := a[j]
+		i := j - 1
+
+		for j := 1; j < len(a); {
+			if i >= 0 && a[i] > key {
+				a[i+1] = a[i]
+				i = i - 1
+
+			} else {
+				break
+
+			}
+			a[i+1] = key
+		}
+
+	}
+
+}
